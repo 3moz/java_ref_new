a9407de5d716e1eabde06b6e1dfaa07c3b7234c8
Eric Moskowitz <moskowitz.e@gmail.com>
2018-08-16 13:55:38 -0400

    throw

---
+++ ThrowDemo.java
@@ -0,0 +1,20 @@
+// demonstrate throw
+
+class ThrowDemo {
+	static void demoproc() {
+		try {
+			throw new NullPointerException("demo");
+		} catch (NullPointerException e) {
+			System.out.println("caught inside demoproc");
+			throw e; //re-throw this exception
+		}
+	}
+	
+	public static void main(String args[]) {
+		try {
+			demoproc();
+		} catch (NullPointerException e) {
+			System.out.println("Recaught: " + e);
+		}
+	}
+}
