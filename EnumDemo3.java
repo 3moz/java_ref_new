b99aeeb5f4cfffce32f629796b711cdbc71fce96
Eric Moskowitz <moskowitz.e@gmail.com>
2018-09-07 17:04:11 -0400

    enum constructor, instance and method

---
+++ EnumDemo3.java
@@ -0,0 +1,29 @@
+// use an enum constructor, instance variable, and method
+enum Apple2 {
+	Jonathan(10),
+	GoldenDel(9),
+	RedDel(12),
+	Winesap(15),
+	Cortland(8);
+	
+	private int price; // price of each apple
+	
+	// constructor
+	Apple2(int p) {price = p;}
+	
+	int getPrice() {return price;}
+}
+
+class EnumDemo3 {
+	public static void main(String args[]) {
+//		Apple2 ap;
+		
+		// display price of Winesap
+		System.out.println("Winesap costs " + Apple2.Winesap.getPrice() + " cents\n");
+		
+		// display all apples and prices
+		System.out.println("All apple prices: ");
+		for(Apple2 a : Apple2.values())
+			System.out.println(a + " costs " + a.getPrice() + " cents");
+	}
+}
